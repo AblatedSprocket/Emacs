bd13361290ab561e0a7f3bca9dfb33829f87ad8e
Andy <andrewwburch@gmail.com>
2021-04-28 10:16:36 -0500

    Added tangle function to create a shell script for dependencies

---
+++ dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# dependencies.sh
+# Don't edit this file, edit config.org' instead ...
+# Auto-generated at Wed Apr 28 2021-04-28T09:58:35 
+  sudo apt install autoconf gnupg2 libgif-dev libgnutls28-dev libgtk-3-dev libjpeg-dev libncurses5-dev libotf-dev libpng-dev librsvg2-dev libtiff-dev libxml2-dev libxpm-dev
+  sudo apt install fonts-inconsolata fonts-hack
+  sudo apt install ripgrep
+  sudo apt install cmake
+  sudo apt install libtool-bin
+  sudo apt install mu4e isync html2text gnupg2
+  sudo apt install sqlite3
+  sudo apt install graphviz
+  sudo apt install fonts-sil-gentium
+  sudo apt install texinfo auctex
+  sudo apt install openjdk-14-jre-headless
+  sudo apt install python3.8 python3-pip
+  pip3 install black
+  pip3 install 'python-language-server[all]'
+  sudo apt install python3-venv
+  sudo apt install bitlbee-dev
--- init.el
+++ init.el
@@ -76,13 +76,55 @@ Note the weekly scope of the command's precision.")
         (insert ";;; config.el ends here"))
       (message "—————• Wrote %s" output-file))))
 
+(defun my-tangle-config-dependencies ()
+  "This function will write source blocks from =config.org= into =dependencies.sh=.
+- not marked as =tangle: no=
+- have a source-code of =shell="
+  (require 'org)
+  (let* ((body-list ())
+         (output-file (concat user-emacs-directory "dependencies.sh"))
+         (org-babel-default-header-args (org-babel-merge-params org-babel-default-header-args
+                                                                (list (cons :tangle output-file)))))
+    (message "—————• Re-generating %s …" output-file)
+    (save-restriction
+      (save-excursion
+        (org-babel-map-src-blocks (concat user-emacs-directory "config.org")
+	  (let* (
+		 (org_block_info (org-babel-get-src-block-info 'light))
+		 (tfile (cdr (assq :tangle (nth 2 org_block_info))))
+		 (match_for_TODO_keyword)
+		 )
+	    (save-excursion
+	      (catch 'exit
+		(org-back-to-heading t)
+		(when (looking-at org-outline-regexp)
+		  (goto-char (1- (match-end 0))))))
+	    (unless (or (string= "no" tfile)
+			(not (string= "shell" lang)))
+	      (add-to-list 'body-list body)
+	      ))))
+      (with-temp-file output-file
+        (insert "#!/bin/bash\n")
+        (insert "# dependencies.sh\n")
+        (insert "# Don't edit this file, edit config.org' instead ...\n")
+        (insert (format "# Auto-generated at %s\n" (format-time-string current-date-time-format (current-time))))
+        (insert (apply 'concat (reverse body-list))))
+      (message "—————• Wrote %s" output-file))))
 
 ;; when config.org is saved, re-generate config.el:
 (defun my-tangle-config-org-hook-func ()
   (when (string= "config.org" (buffer-name))
 	(let ((orgfile (concat user-emacs-directory "config.org"))
-		  (elfile (concat user-emacs-directory "config.el")))
-	  (my-tangle-config-org))))
+	      (elfile (concat user-emacs-directory "config.el"))
+          (shfile (concat user-emacs-directory "dependencies.sh")))
+	  (my-tangle-config-org)
+          (my-tangle-config-dependencies))))
+
+(defun my-tangle-config-dependency-hook-func ()
+  (when (string= "config.org" (buffer-name))
+    (let ((orgfile (concat user-emacs-directory "config.org"))
+          (shfile (concat user-emacs-directory "dependencies.sh")))
+      (my-tangle-config-dependencies))))
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (let ((orgfile (concat user-emacs-directory "config.org"))
@@ -95,16 +137,4 @@ Note the weekly scope of the command's precision.")
 (add-hook 'after-save-hook 'my-tangle-config-org-hook-func)
 
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(simple-httpd elcord cl-lib web-mode ts treemacs-magit svg sqlup-mode smex slack rust-mode rg rainbow-delimiters pyvenv ox-slimhtml ox-jira ox-hugo org-roam-server org-journal org-jira olivetti ob-restclient mw-thesaurus multiple-cursors lsp-ui latex-preview-pane language-detection langtool ivy hl-todo flycheck-rust dap-mode company-quickhelp company-lsp company-auctex blacken bind-key all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;; init.el ends here
