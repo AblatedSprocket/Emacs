8fc55f4ecd57edbcf8285c8f7312313f328957c8
Andy <andrewwburch@gmail.com>
2021-10-03 22:12:39 -0500

    Updated README, removed old files

---
+++ config.sh
@@ -0,0 +1,6 @@
+sudo apt install autoconf gnupg2 libgif-dev libgnutls28-dev libgtk-3-dev libjpeg-dev libncurses5-dev libotf-dev libpng-dev librsvg2-dev libtiff-dev libxml2-dev libxpm-dev
+sudo apt install exiftool vlc
+sudo apt install mu4e isync html2text gnupg2
+sudo apt install sqlite3
+sudo apt install ripgrep
+sudo apt install cmake libtool-bin
